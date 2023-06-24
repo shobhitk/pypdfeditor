c917de6e5bcff5fcd16c6c806783421e40fb3b0a
Shobhit Khinvasara <shobhitk309@gmail.com>
2023-06-24 15:53:40 -0700

    Finally in a good place
    Finally in a good place for me to work

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-# This Python file uses the following encoding: utf-8
+# # This Python file uses the following encoding: utf-8
 import os
 from pathlib import Path
 import sys
 
-from PySide2.QtWidgets import QApplication, QDialog
-from PySide2.QtCore import QFile
-from PySide2.QtUiTools import QUiLoader
+from PyQt6 import QtWidgets, uic
 
+class OutputTreeWidgetItem(QtWidgets.QTreeWidgetItem):
+    def __init__(self, parent=None):
+        super(OutputTreeWidgetItem, self).__init__(parent=parent)
 
-class PyPdfEditor(QDialog):
+class OutputTreeWidget(QtWidgets.QTreeWidget):
+    def __init__(self,parent=None):
+        super(OutputTreeWidget, self).__init__(parent=parent)
+
+class PyPdfEditor(QtWidgets.QDialog):
     def __init__(self):
         super(PyPdfEditor, self).__init__()
-        self.load_ui()
-
-    def load_ui(self):
-        loader = QUiLoader()
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
+        uic.loadUi(path, self)
+        self.output_tree_widget = QtWidgets.QTreeWidget()
+        self.output_tree_frame.layout().addWidget(self.output_tree_widget, 0,0,0,0)
+
+        
+
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QtWidgets.QApplication([])
     widget = PyPdfEditor()
     widget.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
